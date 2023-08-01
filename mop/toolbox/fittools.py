c78fcebf5ea838421487a6d9cd1c575897717cff
mpgh <markus.hundertmark@uni-heidelberg.de>
2023-08-01 20:30:41 +0200

    Merging fit functions and reverting to pspl static fits

--- mop/toolbox/fittools.py
+++ mop/toolbox/fittools.py
@@ -14,16 +14,34 @@ def chi2(params, fit):
 def flux_to_mag(flux):
 
     ZP_pyLIMA = 27.4
-    magnitude = ZP_pyLIMA -2.5*np.log10(flux)
+    magnitude = ZP_pyLIMA - 2.5 * np.log10(flux)
     return magnitude
 
 
-def fit_PSPL_omega2(photometry, emag_limit = None, cores = None):
+def fit_PSPL_omega2(ra, dec, photometry, emag_limit=None):
+    # Default filter sequence for establishing survey dataset
+    filters_order = ['I', 'ip', 'i_ZTF', 'r_ZTF', 'R', 'g_ZTF', 'gp', 'G']
 
+    filters = np.unique(photometry[:, -1])
+    order = []
+    for fil in filters_order:
+
+        mask = np.where(filters == fil)[0]
+        if len(mask) != 0:
+            order += mask.tolist()
+
+    for fil in filters:
+
+        if fil not in filters_order:
+            mask = np.where(filters == fil)[0]
+            if len(mask) != 0:
+                order += mask.tolist()
+
+    filters = filters[order]
     current_event = event.Event()
     current_event.name = 'MOP_to_fit'
     filters = np.unique(photometry[:, -1])
-
+    lightcurve = []
     for ind, filt in enumerate(filters):
 
         if emag_limit:
@@ -45,14 +63,15 @@ def fit_PSPL_omega2(photometry, emag_limit = None, cores = None):
     pspl = PSPL_model.PSPLmodel(current_event, parallax=['None', 0.])
     pspl.define_model_parameters()
     fit_tap = TRF_fit.TRFfit(pspl, loss_function='soft_l1')
+    delta_t0 = 10.
+    default_t0_lower = fit_tap.fit_parameters["t0"][1][0]
+    default_t0_upper = fit_tap.fit_parameters["t0"][1][1]
+    fit_tap.fit_parameters["t0"][1] = [default_t0_lower, default_t0_upper + delta_t0]
     fit_tap.fit_parameters["tE"][1] = [1., 3000.]
-    fit_tap.fit_parameters["u0"][1] = [0., 1.5]
+    fit_tap.fit_parameters["u0"][1] = [0., 2.0]
     fit_tap.fit()
     cov_fit1 = fit_tap.fit_results["covariance_matrix"]
     best_fit1 = fit_tap.fit_results["best_model"]
-    mag_blend_fit = flux_to_mag(fit_tap.fit_results["best_model"][4])
-    mag_source_fit = flux_to_mag(fit_tap.fit_results["best_model"][3])
-    mag_baseline_fit = flux_to_mag(fit_tap.fit_results["best_model"][3] + fit_tap.fit_results["best_model"][4])
     if (np.abs(best_fit1[4]) < 3. * cov_fit1[4, 4] ** 0.5) or\
             (np.abs(best_fit1[3]) < 3. * cov_fit1[3, 3] ** 0.5) or\
             (np.abs(best_fit1[2]) < 3. * cov_fit1[2, 2] ** 0.5):
@@ -60,26 +79,56 @@ def fit_PSPL_omega2(photometry, emag_limit = None, cores = None):
                                     blend_flux_parameter='noblend')
         pspl.define_model_parameters()
         fit_tap = TRF_fit.TRFfit(pspl, loss_function='soft_l1')
+        fit_tap.fit_parameters["t0"][1] = [default_t0_lower, default_t0_upper + delta_t0]
         fit_tap.fit_parameters["tE"][1] = [1., 3000.]
-        fit_tap.fit_parameters["u0"][1] = [0., 1.5]
+        fit_tap.fit_parameters["u0"][1] = [0., 2.0]
         fit_tap.fit()
-        mag_blend_fit = -99.
-        mag_source_fit = flux_to_mag(fit_tap.fit_results["best_model"][4])
+        # default null as in the former implementation
+        mag_blend_fit = "null"
+        mag_source_fit = flux_to_mag(fit_tap.fit_results["best_model"][3])
         mag_baseline_fit = flux_to_mag(fit_tap.fit_results["best_model"][3])
+    else:
+        pspl = PSPL_model.PSPLmodel(current_event, parallax=['None', 0.],
+                                    blend_flux_parameter='noblend')
+        pspl.define_model_parameters()
+        fit_tap = TRF_fit.TRFfit(pspl, loss_function='soft_l1')
+        fit_tap.fit_parameters["t0"][1] = [default_t0_lower, default_t0_upper + delta_t0]
+        fit_tap.fit_parameters["tE"][1] = [1., 3000.]
+        fit_tap.fit_parameters["u0"][1] = [0., 2.0]
+        fit_tap.fit()
+        mag_blend_fit = flux_to_mag(fit_tap.fit_results["best_model"][4])
+        mag_source_fit = flux_to_mag(fit_tap.fit_results["best_model"][3])
+        mag_baseline_fit = flux_to_mag(fit_tap.fit_results["best_model"][3] + fit_tap.fit_results["best_model"][4])
 
     epsilon_numerical_noise = 1e-6
     if np.abs(fit_tap.fit_parameters["u0"][1][0] - fit_tap.fit_results['best_model'][1]) < epsilon_numerical_noise or\
             np.abs(fit_tap.fit_parameters["u0"][1][1] - fit_tap.fit_results['best_model'][1]) < epsilon_numerical_noise or\
             np.abs(fit_tap.fit_parameters["tE"][1][1] - fit_tap.fit_results['best_model'][2]) < epsilon_numerical_noise or\
             np.abs(fit_tap.fit_parameters["tE"][1][1] - fit_tap.fit_results['best_model'][2]) < epsilon_numerical_noise:
-        t0_fit = None
-        u0_fit = None
-        tE_fit = None
-        chi2_fit = None
-
-    t0_fit = fit_tap.fit_results["best_model"][0]
-    u0_fit = fit_tap.fit_results["best_model"][1]
-    tE_fit = fit_tap.fit_results["best_model"][2]
-    chi2_fit = fit_tap.fit_results["chi2"]
-
-    return [t0_fit, u0_fit, tE_fit, mag_source_fit, mag_blend_fit, mag_baseline_fit, chi2_fit]
+        t0_fit = np.nan
+        u0_fit = np.nan
+        tE_fit = np.nan
+        chi2_fit = np.nan
+    else:
+        t0_fit = fit_tap.fit_results["best_model"][0]
+        u0_fit = fit_tap.fit_results["best_model"][1]
+        tE_fit = fit_tap.fit_results["best_model"][2]
+        chi2_fit = fit_tap.fit_results["chi2"]
+    piEN_fit = 0.0
+    piEE_fit = 0.0
+    red_chi2 = chi2_fit / float(len(lightcurve) - 5)
+    # model_telescope = toolbox.fake_telescopes.create_a_fake_telescope()
+    # pyLIMA_parameters = pspl.compute_pyLIMA_parameters(fit_tap.fit_results["best_model"])
+    # model = pspl.compute_the_microlensing_model(model_telescope, pyLIMA_parameters)
+    model_telescope = None
+    try:
+        to_return = [np.around(t0_fit, 3), np.around(u0_fit, 5), np.around(tE_fit, 3), np.around(piEN_fit, 5),
+                     np.around(piEE_fit, 5),
+                     np.around(mag_source_fit, 3), np.around(mag_blend_fit, 3), np.around(mag_baseline_fit, 3),
+                     fit_tap.fit_results["covariance_matrix"], model_telescope, np.around(chi2_fit, 3), red_chi2]
+    except:
+        to_return = [np.around(t0_fit, 3), np.around(u0_fit, 5), np.around(tE_fit, 3), np.around(piEN_fit, 5),
+                     np.around(piEE_fit, 5),
+                     np.around(mag_source_fit, 3), mag_blend_fit, np.around(mag_baseline_fit, 3),
+                     fit_tap.fit_results["covariance_matrix"], model_telescope, np.around(chi2_fit, 3), red_chi2]
+    return to_return
