64ef0422f42fc95b48dc9abc77766b6801de616b
rachel3834 <rstreet@lcogt.net>
2023-09-12 14:59:02 -0700

    Ensured atomic transactions for modeling processes in the background; reconfigured parallelization of needed event modeling only for prod server

--- mop/management/commands/fit_all_events_PSPL.py
+++ mop/management/commands/fit_all_events_PSPL.py
@@ -1,14 +1,9 @@
 from django.core.management.base import BaseCommand
-from tom_dataproducts.models import ReducedDatum
+from django.db import transaction
 from tom_targets.models import Target,TargetExtra
 from astropy.time import Time
-from mop.toolbox import fittools
 from mop.brokers import gaia as gaia_mop
-from mop.toolbox import logs
-import numpy as np
-import datetime
 import random
-import json
 import datetime
 from mop.management.commands.fit_need_events_PSPL import run_fit
 import os
@@ -19,7 +14,7 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
-    help = 'Fit an event with PSPL and parallax, then ingest fit parameters in the db'
+    help = 'Fit a specific selection of events with PSPL and parallax, then ingest fit parameters in the db'
 
     def add_arguments(self, parser):
 
@@ -31,50 +26,56 @@ class Command(BaseCommand):
 
         logger.info('Fitting all events')
 
-        all_events = options['events_to_fit']
+        # Avoid (unlikely but possible) clashing processes hitting the DB at the same time
+        with transaction.atomic():
 
-        if all_events == 'all':
-            list_of_targets = Target.objects.filter()
-        if all_events == 'alive':
-            list_of_targets = Target.objects.filter(targetextra__in=TargetExtra.objects.filter(key='Alive', value=True))
-        if all_events == 'need':
+            # Create a QuerySet which allows us to lock DB rows to avoid clashes
+            qs = Target.objects.select_for_update(skip_locked=True)
 
-            four_hours_ago = Time(datetime.datetime.utcnow() - datetime.timedelta(hours=4)).jd
-            list_of_targets = Target.objects.exclude(targetextra__in=TargetExtra.objects.filter(key='Last_Fit', value_lte=four_hours_ago))
+            # Apply the configured selection of events
+            all_events = options['events_to_fit']
 
-        if all_events[0] == '[':
+            if all_events == 'all':
+                list_of_targets = qs.filter()
+            if all_events == 'alive':
+                list_of_targets = qs.filter(targetextra__in=TargetExtra.objects.filter(key='Alive', value=True))
+            if all_events == 'need':
 
-            years = all_events[1:-1].split(',')
-            events = Target.objects.filter()
-            list_of_targets = []
-            for year in years:
+                four_hours_ago = Time(datetime.datetime.utcnow() - datetime.timedelta(hours=4)).jd
+                list_of_targets = qs.exclude(targetextra__in=TargetExtra.objects.filter(key='Last_Fit', value_lte=four_hours_ago))
 
-                list_of_targets =  [i for i in events if year in i.name]
+            if all_events[0] == '[':
 
-                list_of_targets = list(list_of_targets)
-                random.shuffle(list_of_targets)
+                years = all_events[1:-1].split(',')
+                events = qs.filter()
+                list_of_targets = []
+                for year in years:
 
-                logger.info('Found '+str(len(list_of_targets))+' targets to fit')
+                    list_of_targets =  [i for i in events if year in i.name]
 
-        for target in list_of_targets:
-            # if the previous job has not been started by another worker yet, claim it
+                    list_of_targets = list(list_of_targets)
+                    random.shuffle(list_of_targets)
 
-            print('Working on '+target.name)
-            logger.info('Fitting data for '+target.name)
-            try:
-                if 'Gaia' in target.name:
-                    gaia_mop.update_gaia_errors(target)
+                    logger.info('Found '+str(len(list_of_targets))+' targets to fit')
+
+            for target in list_of_targets:
+                # if the previous job has not been started by another worker yet, claim it
+
+                logger.info('Fitting data for '+target.name)
+                try:
+                    if 'Gaia' in target.name:
+                        gaia_mop.update_gaia_errors(target)
 
 
-                if 'Microlensing' not in target.extra_fields['Classification']:
-                    alive = False
+                    if 'Microlensing' not in target.extra_fields['Classification']:
+                        alive = False
 
-                    extras = {'Alive':alive}
-                    target.save(extras = extras)
-                    logger.info(target.name+' not classified as microlensing')
+                        extras = {'Alive':alive}
+                        target.save(extras = extras)
+                        logger.info(target.name+' not classified as microlensing')
 
-                else:
-                    result = run_fit(target, cores=options['cores'])
+                    else:
+                        result = run_fit(target, cores=options['cores'])
 
-            except:
-                logger.warning('Fitting event '+target.name+' hit an exception')
+                except:
+                    logger.warning('Fitting event '+target.name+' hit an exception')
