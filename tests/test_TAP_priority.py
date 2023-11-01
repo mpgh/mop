827846b65ede882e094ec7f790abd8d26cec182a
Katarzyna Kruszynska <kruszynskakat@gmail.com>
2023-11-01 16:56:16 -0700

    Fixed bad values

--- tests/test_TAP_priority.py
+++ tests/test_TAP_priority.py
@@ -16,7 +16,7 @@ class TestTAPPlanetPriority(TestCase):
         self.target.dec = -35.70483
 
         self.model_params = {'t': 2460199.0,
-                             't0': 2460201.74
+                             't0': 2460201.74,
                              'u0': 0.007,
                              'te': 76.1,
                              'Source_magnitude' : 19.49,
