813ddb9d923f07630eb27489b111e1f403e02c19
rachel3834 <rstreet@lcogt.net>
2023-12-06 09:48:20 -0800

    Add timezone support

--- mop/management/commands/record_scheduled_obs_times.py
+++ mop/management/commands/record_scheduled_obs_times.py
@@ -1,10 +1,13 @@
 from django.core.management.base import BaseCommand
 from tom_observations.models import ObservationRecord
-from django.utils.timezone import make_aware
+from datetime import datetime
+import pytz
 
 class Command(BaseCommand):
 
     help = "Command to back-fill the scheduled start and end times of observation groups"
+
+    tz = pytz.timezone('utc')
 
     def handle(self, *args, **options):
 
@@ -12,6 +15,8 @@ class Command(BaseCommand):
 
         for obs_record in obs_list:
             if not obs_record.scheduled_start or not obs_record.scheduled_end:
+                tstart = None
+                tend = None
                 if 'requests' in obs_record.parameters:
                     # Handle current (2023) format of JSON parameter field
                     if 'windows' in obs_record.parameters['requests'][0].keys():
@@ -23,20 +28,19 @@ class Command(BaseCommand):
                                     tstart = window['start']
                                 if window['end'] > tend:
                                     tend = window['end']
-                        tstart = make_aware(tstart)
-                        tend = make_aware(tend)
-                        obs_record.scheduled_start = tstart
-                        obs_record.scheduled_end = tend
-                        obs_record.save()
-                        
+
                 # Handle older format of parameter dictionary
                 else:
                     if 'start' in obs_record.parameters and 'end' in obs_record.parameters:
-                        tstart = make_aware(obs_record.parameters['start'])
-                        tend = make_aware(obs_record.parameters['end'])
-                        obs_record.scheduled_start = tstart
-                        obs_record.scheduled_end = tend
-                        obs_record.save()
+                        tstart = datetime.strptime(obs_record.parameters['start'], '%Y-%m-%dT%H:%M:%S.%f')
+                        tend = datetime.strptime(obs_record.parameters['end'], '%Y-%m-%dT%H:%M:%S.%f')
 
+                if tstart and tend:
+                    tstart = tstart.replace(tzinfo=tz)
+                    tend = tend.replace(tzinfo=tz)
+                    obs_record.scheduled_start = tstart
+                    obs_record.scheduled_end = tend
+                    obs_record.save()
+                    
             else:
                 print(obs_record.target, obs_record.scheduled_start, obs_record.scheduled_end)
