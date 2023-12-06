20c43623272e2b0323ab1cf68a542b58581907ea
rachel3834 <rstreet@lcogt.net>
2023-12-06 09:31:03 -0800

    Added handling for different parameter dictionary formats

--- mop/management/commands/record_scheduled_obs_times.py
+++ mop/management/commands/record_scheduled_obs_times.py
@@ -12,19 +12,31 @@ class Command(BaseCommand):
 
         for obs_record in obs_list:
             if not obs_record.scheduled_start or not obs_record.scheduled_end:
-                if 'windows' in obs_record.parameters['requests'][0].keys():
-                    tstart = obs_record.parameters['requests'][0]['windows'][0]['start']
-                    tend = obs_record.parameters['requests'][0]['windows'][0]['start']
-                    for req in obs_record.parameters['requests']:
-                        for window in req['windows']:
-                            if window['start'] < tstart:
-                                tstart = window['start']
-                            if window['end'] > tend:
-                                tend = window['end']
-                    tstart = make_aware(tstart)
-                    tend = make_aware(tend)
-                    obs_record.scheduled_start = tstart
-                    obs_record.scheduled_end = tend
-                    obs_record.save()
+                if 'requests' in obs_record.parameters:
+                    # Handle current (2023) format of JSON parameter field
+                    if 'windows' in obs_record.parameters['requests'][0].keys():
+                        tstart = obs_record.parameters['requests'][0]['windows'][0]['start']
+                        tend = obs_record.parameters['requests'][0]['windows'][0]['start']
+                        for req in obs_record.parameters['requests']:
+                            for window in req['windows']:
+                                if window['start'] < tstart:
+                                    tstart = window['start']
+                                if window['end'] > tend:
+                                    tend = window['end']
+                        tstart = make_aware(tstart)
+                        tend = make_aware(tend)
+                        obs_record.scheduled_start = tstart
+                        obs_record.scheduled_end = tend
+                        obs_record.save()
+                        
+                # Handle older format of parameter dictionary
+                else:
+                    if 'start' in obs_record.parameters and 'end' in obs_record.parameters:
+                        tstart = make_aware(obs_record.parameters['start'])
+                        tend = make_aware(obs_record.parameters['end'])
+                        obs_record.scheduled_start = tstart
+                        obs_record.scheduled_end = tend
+                        obs_record.save()
+
             else:
                 print(obs_record.target, obs_record.scheduled_start, obs_record.scheduled_end)
