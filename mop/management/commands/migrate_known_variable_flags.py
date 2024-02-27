75046a6ac9bde4985b48f448283ef97f3e555b69
rachel3834 <rstreet@lcogt.net>
2024-02-27 15:03:32 -0800

    Added catch for mis-specified events

--- mop/management/commands/migrate_known_variable_flags.py
+++ mop/management/commands/migrate_known_variable_flags.py
@@ -31,43 +31,47 @@ class Command(BaseCommand):
                 logger.info('Checking old key ' + old_key)
 
                 extra_pars = t.extra_fields
-                s = SkyCoord(t.ra, t.dec, frame='icrs', unit=(u.deg, u.deg))
+                try:
+                    s = SkyCoord(t.ra, t.dec, frame='icrs', unit=(u.deg, u.deg))
 
-                # If the new key is already set, we assume it is correct
-                if config['new_key'] not in extra_pars.keys():
-                    if old_key in extra_pars.keys() and config['new_key'] not in extra_pars.keys():
-                        status, new_value = self.interpret_entry(extra_pars[old_key])
-                        logger.info(' -> Found existing entry ' + old_key + '=' + str(new_value) + ', status=' + status)
+                    # If the new key is already set, we assume it is correct
+                    if config['new_key'] not in extra_pars.keys():
+                        if old_key in extra_pars.keys() and config['new_key'] not in extra_pars.keys():
+                            status, new_value = self.interpret_entry(extra_pars[old_key])
+                            logger.info(' -> Found existing entry ' + old_key + '=' + str(new_value) + ', status=' + status)
 
-                        # If the existing value is unset, or has an invalid entry, double check
-                        # what it should be
-                        if status == 'unknown':
+                            # If the existing value is unset, or has an invalid entry, double check
+                            # what it should be
+                            if status == 'unknown':
+                                new_value = config['check_function'](s)
+                                logger.info(' -> Unknown status, so checked catalog with result ' + str(new_value))
+
+                        elif old_key not in extra_pars.keys() and config['new_key'] not in extra_pars.keys():
                             new_value = config['check_function'](s)
-                            logger.info(' -> Unknown status, so checked catalog with result ' + str(new_value))
+                            logger.info(' -> No existing entry, so checked catalog with result ' + str(new_value))
 
-                    elif old_key not in extra_pars.keys() and config['new_key'] not in extra_pars.keys():
-                        new_value = config['check_function'](s)
-                        logger.info(' -> No existing entry, so checked catalog with result ' + str(new_value))
+                        ep = TargetExtra.objects.create(
+                            target=t,
+                            key=config['new_key'],
+                            value=new_value
+                        )
+                        ep.save()
+                        logger.info(' -> Created new key ' + config['new_key'] + '=' + str(new_value))
 
-                    ep = TargetExtra.objects.create(
-                        target=t,
-                        key=config['new_key'],
-                        value=new_value
-                    )
-                    ep.save()
-                    logger.info(' -> Created new key ' + config['new_key'] + '=' + str(new_value))
+                    else:
+                        logger.info(' -> Found existing new key entry for ' + config['new_key']
+                                    + '=' + str(extra_pars[config['new_key']]))
 
-                else:
-                    logger.info(' -> Found existing new key entry for ' + config['new_key']
-                                + '=' + str(extra_pars[config['new_key']]))
+                    # Remove the old key entries if available
+                    if old_key in extra_pars.keys():
+                        ep = TargetExtra.objects.get(target=t, key=old_key)
+                        ep.delete()
+                        logger.info(' -> Removed old key ' + old_key + ' entry ' + repr(ep))
 
-                # Remove the old key entries if available
-                if old_key in extra_pars.keys():
-                    ep = TargetExtra.objects.get(target=t, key=old_key)
-                    ep.delete()
-                    logger.info(' -> Removed old key ' + old_key + ' entry ' + repr(ep))
-
-            #opt = input('Continue?')
+                except TypeError:
+                    logger.info('Target ' + t.name + ' has no valid coordinates, skipping')
+                    
+                #opt = input('Continue?')
             #if opt != 'y':
             #    exit()
 
